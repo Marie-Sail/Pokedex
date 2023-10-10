01bf00ed37a0fd93e2c085646e4ed6df5b4cd518
Marie-Sail <marie.saillard.pro@gmail.com>
2023-10-10 15:34:38 +0200

    props

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,25 +1,16 @@
-function PokemonCard() {
-    const pokemon = pokemonList[0];
+function PokemonCard(props) {
+    console.log(props)
+   
 
 
 
     return <figure>
-        {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>Nop</p>}
+        {props.pokemon.imgSrc ? <img src={props.pokemon.imgSrc} /> : <p>Nop</p>}
 
-        <figcaption>{pokemon.name}</figcaption>
+        <figcaption>{props.pokemon.name}</figcaption>
     </figure>;
 }
 
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
 
 
 
